frdeso/lttng-modules

Implement trigger group create
Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
Change-Id: I7c38fbfd26517d00c8de38ca1981da623d570529

src/lttng-events.c
@@ -48,6 +48,7 @@
 #define METADATA_CACHE_DEFAULT_SIZE 4096
 
 static LIST_HEAD(sessions);
+static LIST_HEAD(trigger_groups);
 static LIST_HEAD(lttng_transport_list);
 /*
  * Protect the sessions and metadata caches.
@@ -189,6 +190,57 @@ err:
 	return NULL;
 }
 
+struct lttng_trigger_group *lttng_trigger_group_create(void)
+{
+	struct lttng_transport *transport = NULL;
+	struct lttng_trigger_group *trigger_group;
+	const char *transport_name = "relay-trigger";
+	size_t subbuf_size = 4096;	//TODO
+	size_t num_subbuf = 16;		//TODO
+	unsigned int switch_timer_interval = 0;
+	unsigned int read_timer_interval = 0;
+
+	mutex_lock(&sessions_mutex);
+
+	transport = lttng_transport_find(transport_name);
+	if (!transport) {
+		printk(KERN_WARNING "LTTng transport %s not found\n",
+		       transport_name);
+		goto notransport;
+	}
+	if (!try_module_get(transport->owner)) {
+		printk(KERN_WARNING "LTT : Can't lock transport module.\n");
+		goto notransport;
+	}
+
+	trigger_group = lttng_kvzalloc(sizeof(struct lttng_trigger_group),
+				       GFP_KERNEL);
+	if (!trigger_group)
+		goto nomem;
+
+	trigger_group->ops = &transport->ops;
+	trigger_group->chan = transport->ops.channel_create(transport_name,
+			trigger_group, NULL, subbuf_size, num_subbuf,
+			switch_timer_interval, read_timer_interval);
+	if (!trigger_group->chan)
+		goto create_error;
+
+	trigger_group->transport = transport;
+	list_add(&trigger_group->node, &trigger_groups);
+	mutex_unlock(&sessions_mutex);
+
+	return trigger_group;
+
+create_error:
+	lttng_kvfree(trigger_group);
+nomem:
+	if (transport)
+		module_put(transport->owner);
+notransport:
+	mutex_unlock(&sessions_mutex);
+	return NULL;
+}
+
 void metadata_cache_destroy(struct kref *kref)
 {
 	struct lttng_metadata_cache *cache =
@@ -239,6 +291,19 @@ void lttng_session_destroy(struct lttng_session *session)
 	list_del(&session->list);
 	mutex_unlock(&sessions_mutex);
 	lttng_kvfree(session);
+}
+
+void lttng_trigger_group_destroy(struct lttng_trigger_group *trigger_group)
+{
+	if (!trigger_group)
+		return;
+
+	mutex_lock(&sessions_mutex);
+	trigger_group->ops->channel_destroy(trigger_group->chan);
+	module_put(trigger_group->transport->owner);
+	list_del(&trigger_group->node);
+	mutex_unlock(&sessions_mutex);
+	lttng_kvfree(trigger_group);
 }
 
 int lttng_session_statedump(struct lttng_session *session)

compudj/lttng-modules-dev

src/lttng-events.c
@@ -48,6 +48,7 @@
 #define METADATA_CACHE_DEFAULT_SIZE 4096
 
 static LIST_HEAD(sessions);
+static LIST_HEAD(trigger_groups);
 static LIST_HEAD(lttng_transport_list);
 /*
  * Protect the sessions and metadata caches.
@@ -189,6 +190,57 @@ err:
 	return NULL;
 }
 
+struct lttng_trigger_group *lttng_trigger_group_create(void)
+{
+	struct lttng_transport *transport = NULL;
+	struct lttng_trigger_group *trigger_group;
+	const char *transport_name = "relay-trigger";
+	size_t subbuf_size = 4096;	//TODO
+	size_t num_subbuf = 16;		//TODO
+	unsigned int switch_timer_interval = 0;
+	unsigned int read_timer_interval = 0;
+
+	mutex_lock(&sessions_mutex);
+
+	transport = lttng_transport_find(transport_name);
+	if (!transport) {
+		printk(KERN_WARNING "LTTng transport %s not found\n",
+		       transport_name);
+		goto notransport;
+	}
+	if (!try_module_get(transport->owner)) {
+		printk(KERN_WARNING "LTTng: Can't lock transport module.\n");
+		goto notransport;
+	}
+
+	trigger_group = lttng_kvzalloc(sizeof(struct lttng_trigger_group),
+				       GFP_KERNEL);
+	if (!trigger_group)
+		goto nomem;
+
+	trigger_group->ops = &transport->ops;
+	trigger_group->chan = transport->ops.channel_create(transport_name,
+			trigger_group, NULL, subbuf_size, num_subbuf,
+			switch_timer_interval, read_timer_interval);
+	if (!trigger_group->chan)
+		goto create_error;
+
+	trigger_group->transport = transport;
+	list_add(&trigger_group->node, &trigger_groups);
+	mutex_unlock(&sessions_mutex);
+
+	return trigger_group;
+
+create_error:
+	lttng_kvfree(trigger_group);
+nomem:
+	if (transport)
+		module_put(transport->owner);
+notransport:
+	mutex_unlock(&sessions_mutex);
+	return NULL;
+}
+
 void metadata_cache_destroy(struct kref *kref)
 {
 	struct lttng_metadata_cache *cache =
@@ -243,6 +295,19 @@ void lttng_session_destroy(struct lttng_session *session)
 	list_del(&session->list);
 	mutex_unlock(&sessions_mutex);
 	lttng_kvfree(session);
+}
+
+void lttng_trigger_group_destroy(struct lttng_trigger_group *trigger_group)
+{
+	if (!trigger_group)
+		return;
+
+	mutex_lock(&sessions_mutex);
+	trigger_group->ops->channel_destroy(trigger_group->chan);
+	module_put(trigger_group->transport->owner);
+	list_del(&trigger_group->node);
+	mutex_unlock(&sessions_mutex);
+	lttng_kvfree(trigger_group);
 }
 
 int lttng_session_statedump(struct lttng_session *session)